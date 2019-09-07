d9fb1a94569af2299a99752e6e9aaa6ba49a5b4c
Michael Ion <michael.ion.mi@outlook.com>
2019-09-07 11:31:38 -0400

    created html and css basic layout and assigned javascript variables to the divs

---
+++ script.js
@@ -0,0 +1,13 @@
+const div1 = document.querySelector('#square1');
+const div2 = document.querySelector('.div2');
+const div3 = document.querySelector('.div3');
+const div4 = document.querySelector('.div4');
+const div5 = document.querySelector('.div5');
+const div6 = document.querySelector('.div6');
+const div7 = document.querySelector('.div7');
+const div8 = document.querySelector('.div8');
+const div9 = document.querySelector('.div9');
+
+div1.addEventListener('click', ()=>{
+    div1.innerHTML = "X";
+})
